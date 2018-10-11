6f171e9f3821cc1692734a2bbb79b3db3e363868
WebCheng <webster0808web@gmail.com>
2018-10-11 16:30:47 -0700

    Using numpy to compute normalization data

--- Assignment1/helper.py
+++ Assignment1/helper.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-import numpy as np
-import pandas as pd
+import numpy as np 
 import math
 
 
@@ -26,54 +25,23 @@ def predictVals(thetas, datas):
         rt = np.append(rt, predVal)
     return rt
 
-
 """
 Normalize Values between 0 to 1
 Matrix -> Matrix
 """
 
 
-def NormalizeDatas(matrix):
-    df = pd.DataFrame(matrix)
-    dfNorm = (df - df.min()) / (df.max() - df.min())
+def NormalizeData(matrix):
+    mins = np.min(matrix, axis=0)
+    maxs = np.max(matrix, axis=0)
+    maxs - mins
 
-    # For the first col setting Dummy values( all datas = 1 )
-    if dfNorm.iloc[:, 0].count() == 0:
-        dfNorm.iloc[:, 0] = 1
+    for col in range(1, len(matrix[0])):
+        for row in range(0, len(matrix)):
+            matrix[row][col] = (matrix[row][col] - mins[col]
+                                ) / (maxs[col] - mins[col])
 
-    print("\n ------------ ImportDaTa static detail value------------")
-    print (dfNorm.describe())
-
-    return dfNorm.values
-
-
-"""
-Normalize Values between 0 to 1
-Matrix -> Matrix
-"""
-
-
-# def NormalizeDatasdd(matrix):
-
-#     tMin, tMax, mins, maxs = 0, 0, [], []
-#     for col in range(0, len(matrix)):
-#         for row in range(0, len(matrix[0])):
-#             tMin = row if row <
-#             tMax = row
-#         mins.append(tMin)
-#         maxs.append(tMax)
-
-#     df = pd.DataFrame(matrix)
-#     dfNorm = (df - df.min()) / (df.max() - df.min())
-
-#     # For the first col setting Dummy values( all datas = 1 )
-#     if dfNorm.iloc[:, 0].count() == 0:
-#         dfNorm.iloc[:, 0] = 1
-
-#     print("\n ------------ ImportDaTa static detail value------------")
-#     print dfNorm.describe()
-
-#     return dfNorm.values
+    return matrix
 
 
 """
@@ -106,28 +74,28 @@ def importCsv(path, delimiter=",", isHead=True):
             diff_day = (2018*365 + 5*30 + 31) - (year*365 + month*30 + day)
 
             y.append(float(arr.pop().replace("\n", "")))
-            x.append([float(arr[0])
+            x.append([float(arr[0])  
                     #   ,float(arr[1])
-                    #   ,float(arr[2])
-                      ,diff_day
-                      ,float(arr[3])
-                      ,float(arr[4])
-                      ,float(arr[5])
-                      ,float(arr[6])
-                      ,float(arr[7])
-                      ,float(arr[8])
-                      ,float(arr[9])
-                      ,float(arr[10])
-                      ,float(arr[11])
-                      ,float(arr[12])
-                      ,float(arr[13])
-                      ,float(arr[14])
-                      ,float(arr[15])
-                      ,float(arr[16])
-                      ,float(arr[17])
-                      ,float(arr[18])
-                      ,float(arr[19])
-                      ,float(arr[20])
+                      #   ,float(arr[2])
+                        ,diff_day
+                        ,float(arr[3])
+                        ,float(arr[4])
+                        ,float(arr[5])
+                        ,float(arr[6])
+                        ,float(arr[7])
+                        ,float(arr[8])
+                        ,float(arr[9])
+                        ,float(arr[10])
+                        ,float(arr[11])
+                        ,float(arr[12])
+                        ,float(arr[13])
+                        ,float(arr[14])
+                        ,float(arr[15])
+                        ,float(arr[16])
+                        ,float(arr[17])
+                        ,float(arr[18])
+                        ,float(arr[19])
+                        ,float(arr[20])
                       ])
-
+    
     return [NormalizeDatas(x), y]
